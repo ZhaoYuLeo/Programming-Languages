3abb86c832e5df58e9b20b12c0ddfa1c7a0fa578
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2022-09-04 23:52:32 +0800

    feat: add function string-append-map which appends suffix to a list of strings

--- homework4/hw4.rkt
+++ homework4/hw4.rkt
@@ -5,8 +5,11 @@
 
 ;; put your code below
 
-;; produces a list of numbers from low to high(including low and possibly high) separated by stride and in sorted order
+;; Problem 1: Produces a list of numbers from low to high(including low and possibly high) separated by stride and in sorted order
 (define (sequence low high stride)
   (if (> low high)
       null
       (cons low (sequence (+ low stride) high stride))))
+
+;; Problem 2: Returns a list of strings. Each element of the output should be the corresponding element of the input appended with suffix (with no extra space between the element and suffix).
+(define (string-append-map xs suffix) (map (lambda (x) (string-append x suffix)) xs))
