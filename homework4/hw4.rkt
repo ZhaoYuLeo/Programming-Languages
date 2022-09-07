b6190baa3a522694ed76b084cad75daccb2ff39d
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2022-09-07 23:58:02 +0800

    feat: return nth element of the given list

--- homework4/hw4.rkt
+++ homework4/hw4.rkt
@@ -5,11 +5,23 @@
 
 ;; put your code below
 
-;; Problem 1: Produces a list of numbers from low to high(including low and possibly high) separated by stride and in sorted order
+;; Problem 1: Produces a list of numbers from low to high(including low
+;; and possibly high) separated by stride and in sorted order
 (define (sequence low high stride)
   (if (> low high)
       null
       (cons low (sequence (+ low stride) high stride))))
 
-;; Problem 2: Returns a list of strings. Each element of the output should be the corresponding element of the input appended with suffix (with no extra space between the element and suffix).
-(define (string-append-map xs suffix) (map (lambda (x) (string-append x suffix)) xs))
+;; Problem 2: Returns a list of strings. Each element of the output should
+;; be the corresponding element of the input appended with suffix (with no
+;; extra space between the element and suffix).
+(define (string-append-map xs suffix)
+  (map (lambda (x) (string-append x suffix)) xs))
+
+;; Problem 3: Returns the ith element of the given list where we count from
+;; zero and i is the remainder produced when dividing n by the list's length
+;; raise (error "list-nth-mod: negative number") when the given number is
+;; negative raise (error "list-nth-mod: empty list") when the given list is
+;; empty.
+(define (list-nth-mod xs n)
+  (car (list-tail xs (remainder n (length xs)))))
