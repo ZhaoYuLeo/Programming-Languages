7db86ed110e12d01ed1bcbd92d20cdc7e18b8659
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2022-09-12 22:18:24 +0800

    feat: add function fibonacc which is a stream of fibonacc numbers with first element 0

--- homework4/hw4extra.rkt
+++ homework4/hw4extra.rkt
@@ -19,3 +19,10 @@
                                           acc
                                           (sum-map (cdr xs) (cdr ys) (cons (+ (car xs) (car ys)) acc))))])
     (sum-map xs (invert xs null) null)))
+
+;; Problem2 : This is a stream, the first element of which is 0, the second one
+;; is 1, and each successive element is the sum of two immediately pre-
+;; ceding elements.
+(define fibonacc
+         (letrec ([s (lambda (acc1 acc2) (cons (+ acc1 acc2) (lambda () (s (+ acc1 acc2) acc1))))])
+           (lambda () (cons 0 (lambda () (cons 1 (lambda () (s 1 0)))))))); the key point of stream is thunk
